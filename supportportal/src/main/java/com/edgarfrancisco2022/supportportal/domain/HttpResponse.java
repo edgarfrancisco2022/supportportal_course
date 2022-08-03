a3f0662dd5d85f320b60587ef967cf6a55363aaf
Edgar Pérez <101810702+edgarfrancisco2022@users.noreply.github.com>
2022-08-03 12:59:39 -0500

    initial commit

---
+++ supportportal/src/main/java/com/edgarfrancisco2022/supportportal/domain/HttpResponse.java
@@ -0,0 +1,54 @@
+package com.edgarfrancisco2022.supportportal.domain;
+
+import org.springframework.http.HttpStatus;
+
+public class HttpResponse {
+    //HTTP Custom Response Class
+
+    private int httpStatusCode;
+    private HttpStatus httpStatus; // ENUM
+    private String reason;
+    private String message;
+
+    public HttpResponse() {
+    }
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public void setHttpStatusCode(int httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
