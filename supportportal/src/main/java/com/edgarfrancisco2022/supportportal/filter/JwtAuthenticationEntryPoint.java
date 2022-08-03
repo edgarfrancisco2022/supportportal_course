c8d401c37a3886b964aaa4da05600f0ab41860b2
Edgar Pérez <101810702+edgarfrancisco2022@users.noreply.github.com>
2022-08-03 17:54:02 -0500

    add JwtAuthorizationFilter - OncePerRequestFilter

---
+++ supportportal/src/main/java/com/edgarfrancisco2022/supportportal/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,6 @@
+package com.edgarfrancisco2022.supportportal.filter;
+
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+}
